0dd09b42806962897c7ec8cf5ea5dea6ba87d625
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-01-20 12:58:41 +0530

    dublicate and search in list

---
+++ Praticals/31/Dublicate_Search.py
@@ -0,0 +1,18 @@
+# Q.3 (i) Write a Programme to check if an element exists in the list or not. If that element does not exist then add that particular element in the List.
+# (ii) Write a Programme to check if there is a duplicate element Present in the list or not.
+# If that is present in the list then remove it from the List.
+
+list = [1,2,3,4,4,5,6,7,7]
+
+s = set(list)
+l=[]
+
+num =int(input("Enter the number to check"))
+
+if(num in list):
+  print("Number is present")
+else:
+   print("Number is not present")
+   list.append(num)
+   print("List after adding the number")
+   print(list)
